58c34fe61b29346dce884e81a8c9f95524afe3ca
robert-virtual <robertocastill63@gmail.com>
2022-02-07 09:36:50 -0600

    route auth name changed⚡

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3001;
 app.use(express.json());
 app.use(cors());
 
-app.use("/auth", require("./routes/auth"));
+app.use(require("./routes/auth"));
 
 app.listen(port, () => {
   console.log(`server running on port  ${port}...`);
